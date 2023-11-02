a34d3134c8636b25ab9f7ac014e34c9aa5a0041e
marionsmlg <marion.schimmerling2@hotmail.fr>
2023-11-02 16:50:14 +0100

    add contact in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,17 +1,18 @@
 <script setup>
-import { defineComponent, h } from 'vue'
-import LogoPurecare from '@/components/PurecareLogo.vue'
+import { defineComponent, h } from "vue";
+import LogoPurecare from "@/components/PurecareLogo.vue";
+import { EnvelopeIcon } from "@heroicons/vue/24/outline";
 
 const navigation = {
   about: [
-    { name: 'Notre mission', href: '#' },
-    { name: 'FAQ', href: '#' }
+    { name: "Notre mission", href: "#" },
+    { name: "FAQ", href: "#" },
   ],
   legal: [
-    { name: 'CGU', href: '#' },
-    { name: 'Politique de confidentialité', href: '#' },
-    { name: 'Mentions légales', href: '#' },
-    { name: 'Cookies', href: '#' }
+    { name: "CGU", href: "#" },
+    { name: "Politique de confidentialité", href: "#" },
+    { name: "Mentions légales", href: "#" },
+    { name: "Cookies", href: "#" },
   ],
 
   social: [
@@ -44,21 +45,21 @@ const navigation = {
     //   })
     // },
     {
-      name: 'GitHub',
-      href: 'https://github.com/marionsmlg/biocare-vue',
+      name: "GitHub",
+      href: "https://github.com/marionsmlg/biocare-vue",
       icon: defineComponent({
         render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-            h('path', {
-              'fill-rule': 'evenodd',
-              d: 'M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z',
-              'clip-rule': 'evenodd'
-            })
-          ])
-      })
-    }
-  ]
-}
+          h("svg", { fill: "currentColor", viewBox: "0 0 24 24" }, [
+            h("path", {
+              "fill-rule": "evenodd",
+              d: "M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z",
+              "clip-rule": "evenodd",
+            }),
+          ]),
+      }),
+    },
+  ],
+};
 </script>
 
 <template>
@@ -68,9 +69,17 @@ const navigation = {
       class="mx-auto max-w-7xl px-6 pb-8 pt-16 lg:px-8 border border-transparent border-t-gray-300 mt-4"
     >
       <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <div class="space-y-8">
+        <div class="space-y-6">
           <LogoPurecare class="w-40" />
-          <p class="text-sm leading-6 text-gray-600">Des soins sains pour une beauté durable.</p>
+          <p class="text-sm leading-6 text-gray-600">
+            Des soins sains pour une beauté durable.
+          </p>
+          <div class="flex items-center text-gray-400 space-x-4">
+            <EnvelopeIcon class="h-5 w-5" />
+            <span class="text-xs leading-6 text-gray-600"
+              >purecare06@gmail.com</span
+            >
+          </div>
           <div class="flex space-x-6">
             <a
               v-for="item in navigation.social"
@@ -83,10 +92,13 @@ const navigation = {
             </a>
           </div>
         </div>
-        <!-- <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
+        <!-- 
+        <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
           <div class="md:grid md:grid-cols-2 md:gap-8">
             <div>
-              <h3 class="text-sm font-semibold leading-6 text-gray-900">À propos</h3>
+              <h3 class="text-sm font-semibold leading-6 text-gray-900">
+                À propos
+              </h3>
               <ul role="list" class="mt-6 space-y-4">
                 <li v-for="item in navigation.about" :key="item.name">
                   <a
@@ -98,7 +110,9 @@ const navigation = {
               </ul>
             </div>
             <div class="mt-10 md:mt-0">
-              <h3 class="text-sm font-semibold leading-6 text-gray-900">Informations légales</h3>
+              <h3 class="text-sm font-semibold leading-6 text-gray-900">
+                Informations légales
+              </h3>
               <ul role="list" class="mt-6 space-y-4">
                 <li v-for="item in navigation.legal" :key="item.name">
                   <a
